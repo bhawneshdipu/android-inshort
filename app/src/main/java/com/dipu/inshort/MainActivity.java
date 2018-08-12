dc0d3aea1749a5dae7127f9bb81c370c175a8603
bhawneshdipu <bhawnesh.dipu@gmail.com>
2018-08-12 16:45:42 +0530

    initial commit

---
+++ app/src/main/java/com/dipu/inshort/MainActivity.java
@@ -0,0 +1,20 @@
+package com.dipu.inshort;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.*;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initSwipePager();
+    }
+
+    private void initSwipePager(){
+        VerticalViewPager verticalViewPager = (VerticalViewPager) findViewById(R.id.vPager);
+        verticalViewPager.setAdapter(new VerticlePagerAdapter(this));
+    }
+}
